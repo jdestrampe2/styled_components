29c03b0c8198f3ee6b0e0409e0cf1c4097af3ca4
Joseph Destrampe <gaiagreenworks@gmail.com>
2021-05-16 16:06:55 -0700

    Tenth Commit. Added login form fields for username and password

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,4 @@
 export * from '../common/Button.js';
 export * from '../common/PageLayout.js';
 export * from '../common/Header.js';
+export * from '../common/Input.js';
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,5 +2,12 @@ import React from 'react';
 import { PageLayout } from '../../components/common/PageLayout.js';
 
 export default function Home() {
-	return <PageLayout>Home</PageLayout>;
+	return (
+	<PageLayout>
+		<h1>Home</h1>
+		<p>
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor possimus quod laborum totam dolore laboriosam, similique doloremque libero commodi nemo deleniti voluptatibus delectus explicabo accusamus consequatur culpa ipsa et quisquam facere natus aperiam! A fugiat, dicta reprehenderit voluptates amet, perspiciatis dolor neque nulla ea deleniti mollitia vero? Ipsa tempora distinctio doloremque veniam tenetur. Qui aliquam iusto dicta, et iure, numquam accusantium porro quasi velit distinctio nostrum autem recusandae dolorem optio. Ipsa facere eius corrupti voluptatibus modi nam veritatis cum? Molestiae modi illum voluptatum porro non neque facilis, asperiores dicta aliquam suscipit ratione laboriosam quis atque voluptate, aut excepturi sint commodi!
+		</p>
+	</PageLayout>
+	);
 }
